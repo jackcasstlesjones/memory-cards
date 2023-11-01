2552228f5281c735440aa46cedd893c72f72c0fd
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-11-01 15:51:57 +1100

    add basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,74 @@ import uuid from "react-uuid";
 import "./App.css";
 
 function App() {
-  const [dogSrc, setDogSrc] = useState("");
+  const [gifSrc, setGifSrc] = useState("");
 
-  const searchTerm = "dogs";
+  const [numClicks, setNumClick] = useState(0);
+
+  const [clickedPics, setClickedPics] = useState([]);
+
+  const onClick = (thisID) => {
+    setClickedPics((prevPics) => [...prevPics, thisID]);
+    console.log(clickedPics);
+    if (clickedPics.includes(thisID)) {
+      alert("You Lose!!!");
+    }
+    setNumClick((prevCount) => prevCount + 1);
+  };
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  const searchTerm = "succession";
   useEffect(() => {
     const getData = async () => {
       const myData = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=12`,
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=9`,
         { mode: "cors" }
       );
 
       const goodData = await myData.json();
-      setDogSrc(goodData.data);
-      console.log(dogSrc);
+
+      shuffle(goodData.data);
+      setGifSrc(goodData.data);
     };
 
     getData();
     return () => {
-      setDogSrc("");
+      setGifSrc("");
     };
-  }, []);
+  }, [numClicks]);
 
   return (
     <>
+      <h1>Current Score: {numClicks}</h1>
       <div className="dog-container">
-        {dogSrc.length > 0 &&
-          dogSrc.map((gif) => {
-            return <img id="poo" key={"poo"} src={gif.images.original.url} />;
+        {gifSrc.length > 0 &&
+          gifSrc.map((gif) => {
+            return (
+              <img
+                onClick={() => onClick(gif.id)}
+                key={gif.id}
+                src={gif.images.original.url}
+              />
+            );
           })}
       </div>
     </>
